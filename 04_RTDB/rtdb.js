769fc3954d3d49fdff644f4ecb05f2c295ff4ef2
Andre Baltieri <andrebaltieri@Andres-MacBook-Pro.local>
2016-07-08 08:04:47 -0300

    Adicionado demo de real time database

--- 04_RTDB/rtdb.js
+++ 04_RTDB/rtdb.js
@@ -7,23 +7,31 @@ var config = {
 };
 firebase.initializeApp(config);
 
-// Referências
-var provider = new firebase.auth.GithubAuthProvider();
-var authButton = document.getElementById('authButton');
-var displayName = document.getElementById('displayName');
+var usersList = document.getElementById('usersList');
+var nameInput = document.getElementById('nameInput');
+var ageInput = document.getElementById('ageInput');
+var addButton = document.getElementById('addButton');
 
 // Ao clicar no botão, autentica
-authButton.addEventListener('click', function () {
-    firebase.auth()
-        .signInWithPopup(provider)
-        .then(function (result) {
-            var token = result.credential.accessToken;
-            displayName.innerText = 'Bem vindo, ' + result.user.displayName;
-            authButton.disabled = true;
-        }).catch(function (error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            var email = error.email;
-            var credential = error.credential;
-        });
+addButton.addEventListener('click', function () {        
+    create(nameInput.value, ageInput.value);
+});
+
+// Função para criar um registro no Firebase
+function create(name, age) {
+    var data = {
+        name: name,
+        age: age
+    };
+
+    return firebase.database().ref().child('users').push(data);
+}
+
+firebase.database().ref('users').on('value', function (snapshot) {
+    usersList.innerHTML = '';
+    snapshot.forEach(function (item) {
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(item.val().name + ': ' + item.val().age));
+        usersList.appendChild(li);
+    });
 });
